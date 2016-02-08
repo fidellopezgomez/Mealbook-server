0a0f14448284bc14d2b67ca92be37e92629d7daf
Fidel <fidellopezgomez@gmail.com>
2016-02-09 00:41:59 +0100

    Add settings page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var settings = require('../controllers/settings');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mealbook' });
+  res.render('index', { user_name: 'Fidel', title: 'Mealbook' });
 });
+
+router.get('/settings',settings.index);
+router.get('/settings/resume',settings.resume);
 
 module.exports = router;
