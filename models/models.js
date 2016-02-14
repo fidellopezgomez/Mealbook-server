a639b613e29ef57f8dca49d2bfcd5c49542e9dd4
Fidel <fidellopezgomez@gmail.com>
2016-02-14 14:24:20 +0100

    Update models.js

--- models/models.js
+++ models/models.js
@@ -1,18 +1,5 @@
 var path = require('path');
 
-var pg = require('pg');
-
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if(err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;');
-    .on('row',function(row) {
-      console.lg(JSON.stringify(row));
-    })
-});
-
 // Postgres DATABASE_URL = postgres://user:password@host:port/DATABASE_URL
 // SQLite DATABASE_URL = sqlite://:@:/
 var URL = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
